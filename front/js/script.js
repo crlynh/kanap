b04217f5259c747427ca9b6aa1a93a9a2c39e19c
carolinesom <somcaroline@gmail.com>
2022-05-16 17:56:44 +0200

    script js

---
+++ front/js/script.js
@@ -0,0 +1,41 @@
+getProducts();
+addProducts([]);
+
+//Récupération de la liste des produits
+function getProducts() {
+  fetch("http://localhost:3000/api/products")
+  .then((res) => res.json())
+  .then((data) => addProducts(data))
+}
+
+//Ajout de la liste des produits
+function addProducts(canapes) {
+  for (let i=0; i < canapes.length; i++) {
+  const canape = canapes[i];
+  
+  // Création de l'élément "a"
+  let productLink = document.createElement("a");
+  productLink.href = "./product.html?id=" + canapes[i]._id
+  document.getElementById("items").append(productLink);
+
+  // Création de l'élément "article"
+  let productArticle = document.createElement("article");
+  productLink.appendChild(productArticle);
+
+  // Création de l'élément "img"
+  let productImg = document.createElement("img");
+  productArticle.appendChild(productImg);
+  productImg.src = canapes[i].imageUrl;
+  productImg.alt = canapes[i].altTxt;
+
+  // Création de l'élément titre "h3"
+  let productTitle = document.createElement("h3");
+  productArticle.appendChild(productTitle);
+  productTitle.textContent = canapes[i].name;
+
+  // Création de l'élément description "p"
+  let productDescription = document.createElement("p");
+  productArticle.appendChild(productDescription);
+  productDescription.textContent = canapes[i].description
+  }
+}
